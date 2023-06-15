27a26c11c8a7678664c739d79125cb7fb850a8c3
idil <idilcalislar@gmail.com>
2023-06-15 16:55:17 +0200

    add 4th task  search bar functionality

--- components/card/card.js
+++ components/card/card.js
@@ -18,7 +18,7 @@ export function createCharacterCard(character) {
         <dt class="card__info-title">Type</dt>
         <dd class="card__info-description"></dd>
         <dt class="card__info-title">Occurrences</dt>
-        <dd class="card__info-description">${character.episode.length}</dd>
+        <dd class="card__info-description">${character.occurrences}</dd>
       </dl>
     </div>
 `;
